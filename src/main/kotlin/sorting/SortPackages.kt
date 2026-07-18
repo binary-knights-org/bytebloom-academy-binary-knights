1c7a7268e5401c88a59fbf85e2f15e5d165a6e01
Ahmed Farani <am.farani@std.alaqsa.edu.ps>
2026-07-18 21:30:53 +0300

    feat: implement compound selection sort for packages by priority and weight

---
+++ src/main/kotlin/sorting/SortPackages.kt
@@ -0,0 +1,47 @@
+package sorting
+import models.Package
+
+fun priorityValue(priority: String): Int {
+    return when (priority.uppercase()) {
+        "URGENT" -> 3
+        "STANDARD" -> 2
+        "LOW" -> 1
+        else -> 1
+    }
+}
+
+fun sortPackagesByPriorityAndWeight(packages: List<Package>): List<Package> {
+    val sortedPackages = packages.toMutableList()
+    val packageCount = sortedPackages.size
+
+    for (i in 0 until packageCount - 1) {
+        var bestIndex = i
+        var currentHighestPriority = priorityValue(sortedPackages[bestIndex].priority)
+        var currentHighestWeight = sortedPackages[bestIndex].weight
+
+        for (j in i + 1 until packageCount) {
+            val scanPriority = priorityValue(sortedPackages[j].priority)
+            val scanWeight = sortedPackages[j].weight
+
+            if (scanPriority > currentHighestPriority) {
+                bestIndex = j
+                currentHighestPriority = scanPriority
+                currentHighestWeight = scanWeight
+
+            } else if (scanPriority == currentHighestPriority) {
+                if (scanWeight > currentHighestWeight) {
+                    bestIndex = j
+                    currentHighestPriority = scanPriority
+                    currentHighestWeight = scanWeight
+                }
+            }
+        }
+        if (bestIndex != i) {
+            val packageToSwap = sortedPackages[bestIndex]
+            sortedPackages[bestIndex] = sortedPackages[i]
+            sortedPackages[i] = packageToSwap
+        }
+    }
+    
+    return sortedPackages
+}
