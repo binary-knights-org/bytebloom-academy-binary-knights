cf04dadc183cc11b257c8db01876e4bd8008fbd5
pc <ks.ashour@std.alaqsa.edu.ps>
2026-07-19 13:09:29 +0300

    feat: Integrate parsing and sorting, report successful records, and display top 3 packages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,63 @@
-package org.example
+import Logic.loadFleetData
+import org.example.Logic.parseWarehouses
+
+import Logic.loadFleetData
+import Logic.parsePackage
+import org.example.Logic.parseWarehouses
+import routes.parseRoutes
+import sorting.sortPackagesByPriorityAndWeight
+import java.io.File
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val packageFilePath = "src/main/resources/packages.csv"
+    val warehousesFilePath = "src/main/resources/warehouses.csv"
+    val routesFilePath = "src/main/resources/routes.csv"
+    val fleetFilePath = "src/main/resources/fleet.csv"
+
+
+
+    val packageList = mutableListOf<models.Package>()
+    val packageFile = java.io.File(packageFilePath)
+    if (packageFile.exists()) {
+        val lines = packageFile.readLines()
+        for (i in 1 until  lines.size){
+            val line = lines[i].trim()
+            if (line.isEmpty()) continue
+            val parsedPackage = parsePackage(line)
+            if (parsedPackage != null) {
+                packageList.add(parsedPackage)
+            }
+
+        }
     }
+
+
+
+    val warehousesList = parseWarehouses(warehousesFilePath)
+    val routesList = parseRoutes(routesFilePath)
+    val fleetList = loadFleetData(fleetFilePath)
+    val sortedPackages = sortPackagesByPriorityAndWeight(packageList)
+
+
+    println("\n---  Data Parsing Report ---")
+    println(" Successfully parsed Packages: ${packageList.size} records.")
+    println(" Successfully parsed Warehouses: ${warehousesList.size} records.")
+    println(" Successfully parsed Routes: ${routesList.size} records.")
+    println(" Successfully parsed Fleet: ${fleetList.size} vehicle records.")
+
+
+    println("\n---  Executing Manual Package Sorting ---")
+
+    println("\n---  Top 3 Priority Shipments ---")
+
+    var packageCount = 1
+    for (pkg in sortedPackages) {
+        if (packageCount >= 4) break
+        println("package = $packageCount , id = ${pkg.id} , destinationHub = ${pkg.destinationHubId} , weight = ${pkg.weight} kg , priority = ${pkg.priority}")
+        packageCount++
+    }
+
 }
