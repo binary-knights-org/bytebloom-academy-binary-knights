cbf728e5310e6d1026bbcc6bbd640f9ef312f2c6
SabahBaraka <barakasabah844@gmail.com>
2026-07-28 18:27:03 +0300

    Update CSV files & dataholder: add new column in dataholder like latitudev , longitude in WarehouseRaw.kt and originHubId in PackageRaw.kt

--- src/main/kotlin/dataholder/PackageRaw.kt
+++ src/main/kotlin/dataholder/PackageRaw.kt
@@ -1,8 +1,11 @@
 package dataholder
 
+import javax.lang.model.util.Elements
+
 data class PackageRaw(
     val packageId: String,
     val weight: Double,
+    val originHubId: String,
     val destinationHubId: String,
     val priority: String
 )
