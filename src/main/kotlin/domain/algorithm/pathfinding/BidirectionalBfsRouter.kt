fa377bcb81305d02791eda1dc6089ca19d63f977
Ahmed Farani <am.farani@std.alaqsa.edu.ps>
2026-08-18 18:07:01 +0300

    feat(pathfinding): comment 2 variable to avoid error detekt

--- src/main/kotlin/domain/algorithm/pathfinding/BidirectionalBfsRouter.kt
+++ src/main/kotlin/domain/algorithm/pathfinding/BidirectionalBfsRouter.kt
@@ -5,8 +5,8 @@ import domain.model.Warehouse
 class BidirectionalBfsRouter : ShortestPathRouter {
     override fun findShortestPath(origin: Warehouse, destination: Warehouse): List<Warehouse>? {
         if (origin == destination) return listOf(origin)
-        val forwardState = createInitialState(origin)
-        val backwardState = createInitialState(destination)
+//        val forwardState = createInitialState(origin)
+//        val backwardState = createInitialState(destination)
         return null
     }
 
