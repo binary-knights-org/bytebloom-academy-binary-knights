1220a8a33451b08feb50c7fc659f5703c9463561
Ahmed Farani <am.farani@std.alaqsa.edu.ps>
2026-08-20 18:24:48 +0300

    fix: isolate route repository contract from data layer

--- src/main/kotlin/domain/repository/RouteRepository.kt
+++ src/main/kotlin/domain/repository/RouteRepository.kt
@@ -1,7 +1,13 @@
 package domain.repository
 
-import data.dataholder.RouteRaw
-
 interface RouteRepository {
-    fun getAllRoutes(): List<RouteRaw>
+    data class RouteRecord(
+        val routeId: String,
+        val originHubId: String,
+        val destinationHubId: String,
+        val distanceKm: Double,
+        val typicalDelayMin: Int
+    )
+
+    fun getAllRoutes(): List<RouteRecord>
 }
