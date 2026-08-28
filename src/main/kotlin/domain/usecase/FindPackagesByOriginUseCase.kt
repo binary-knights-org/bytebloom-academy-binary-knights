7600c400d24e1a634004961d0d2e632ca26abac8
fk42117638-hub <fk42117638@gmail.com>
2026-08-28 16:01:13 +0300

    feat: add FindPackagesByOriginUseCase

---
+++ src/main/kotlin/domain/usecase/FindPackagesByOriginUseCase.kt
@@ -0,0 +1,13 @@
+package domain.usecase
+
+import domain.model.Package
+import domain.model.Warehouse
+import domain.repository.PackageRepository
+
+class FindPackagesByOriginUseCase(
+    private val packageRepository: PackageRepository
+) {
+    operator fun invoke(originHub: Warehouse): List<Package> {
+        return packageRepository.getAllPackages().filter { it.originHub == originHub }
+    }
+}
