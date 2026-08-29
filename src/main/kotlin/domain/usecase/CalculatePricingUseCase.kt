491e65ae528ddb9dbe6611ed24f747d17745e594
pc <ks.ashour@std.alaqsa.edu.ps>
2026-08-29 12:57:23 +0300

    feat(usecase): add CalculatePricingUseCase

---
+++ src/main/kotlin/domain/usecase/CalculatePricingUseCase.kt
@@ -0,0 +1,17 @@
+package domain.usecase
+
+import domain.model.PackageComponent
+import domain.model.Route
+import domain.pricing.DispatchStrategy
+import domain.pricing.RoutePricingEngine
+
+class CalculatePricingUseCase(
+    private val pricingEngine : RoutePricingEngine
+) {
+    operator fun invoke(pkg : PackageComponent ,route: Route , strategy: DispatchStrategy ): Double {
+    pricingEngine.setStrategy(strategy)
+    val baseCost =  pricingEngine.calculateCost(pkg.weight,route.distanceKm)
+    return  pkg.calculateTransitRate(baseCost)
+    }
+
+}
