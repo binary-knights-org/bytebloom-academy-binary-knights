53caf2ac478206ed1eed45c572eecd71dc037b75
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-07 17:43:49 +0300

    fix: resolve detekt

--- src/main/kotlin/ui/Main.kt
+++ src/main/kotlin/ui/Main.kt
@@ -20,9 +20,6 @@ fun main() {
     val packageRepository = data.repository.CsvPackageRepository(PACKAGE_FILE_PATH, warehouseRepository)
     val vehicleRepository = data.repository.CsvVehicleRepository(VEHICLES_FILE_PATH, warehouseRepository)
     val routeRepository = data.repository.CsvRouteRepository(ROUTES_FILE_PATH, warehouseRepository)
-
-    printParsingReport(vehicleRepository, warehouseRepository, packageRepository, routeRepository)
-
     val graph = buildDomainGraph(vehicleRepository, warehouseRepository, packageRepository, routeRepository)
     val findPackagesForConsolidationUseCase = FindPackagesForConsolidationUseCase(packageRepository)
     val findSuitableVehicleUseCase = FindSuitableVehicleUseCase(vehicleRepository)
@@ -32,11 +29,14 @@ fun main() {
     val findBidirectionalRouteUseCase = FindBidirectionalRouteUseCase(BidirectionalBfsRouter(warehouseRepository))
     val calculatePricingUseCase = CalculatePricingUseCase(RoutePricingEngine(EcoStrategy()))
 
+    printParsingReport(vehicleRepository, warehouseRepository, packageRepository, routeRepository)
     runCargoDemos(packageRepository, graph)
-    runPackageConsolidationDemo(findPackagesForConsolidationUseCase, findSuitableVehicleUseCase, assignPackagesToVehicleUseCase)
+    runPackageConsolidationDemo(
+        findPackagesForConsolidationUseCase, findSuitableVehicleUseCase, assignPackagesToVehicleUseCase)
     runPricingAndDecoratorDemos(graph, calculatePricingUseCase)
     runBreakdownSimulationDemo()
-    runRoutingAndComparisonDemos(warehouseRepository, graph, findOptimalPathUseCase, findFewestHopsRouteUseCase, findBidirectionalRouteUseCase)
+    runRoutingAndComparisonDemos(
+        warehouseRepository, graph, findOptimalPathUseCase, findFewestHopsRouteUseCase, findBidirectionalRouteUseCase)
     runSimulationDemos(graph)
     printSystemFooter()
 }
