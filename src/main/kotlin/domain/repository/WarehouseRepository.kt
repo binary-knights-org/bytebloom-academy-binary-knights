6b247d30da01272d0779d52045cdefafe14ee3f7
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-07 17:29:25 +0300

    refactor(repository): update WarehouseRepository interface package to domain

--- src/main/kotlin/domain/repository/WarehouseRepository.kt
+++ src/main/kotlin/domain/repository/WarehouseRepository.kt
@@ -1,7 +1,13 @@
 package domain.repository
 
 import domain.model.Warehouse
+import domain.model.Package
+import domain.model.Route
+import domain.model.Vehicle
 
 interface WarehouseRepository {
-   fun getAllWarehouses(): List<Warehouse>
+    fun getAllWarehouses(): List<Warehouse>
+    fun linkWarehouseData(
+        packages: List<Package>, vehicles: List<Vehicle>, routes: List<Route>
+    ): List<Warehouse>
 }
