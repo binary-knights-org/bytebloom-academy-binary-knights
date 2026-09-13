d4ac831536d29fcd89bdd57b77463b643138a21f
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-13 19:39:32 +0300

    fix: edit import statement

--- src/main/kotlin/data/remote/SupabaseHttpClient.kt
+++ src/main/kotlin/data/remote/SupabaseHttpClient.kt
@@ -1,13 +1,19 @@
 package data.remote
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.patch
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 object SupabaseHttpClient {
