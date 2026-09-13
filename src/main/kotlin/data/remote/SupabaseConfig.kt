db0832385d325e1e1fb5b0f44d9e2a946fdb3f3d
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-13 20:33:49 +0300

    refactor: ignore local.properties to prevent API key leaks

--- src/main/kotlin/data/remote/SupabaseConfig.kt
+++ src/main/kotlin/data/remote/SupabaseConfig.kt
@@ -1,7 +1,18 @@
 package data.remote
 
+import java.io.File
+import java.util.Properties
+
 object SupabaseConfig {
-    const val BASE_URL = "https://epqowrtlreysnwpuhaqf.supabase.co/rest/v1"
-    const val API_KEY = "sb_publishable_o2ZrTDEnHgHyrr4xqheCtg_BdNmWKMx"
-    const val BEARER_TOKEN = API_KEY
+
+    private val properties = Properties().apply {
+        val file = File("local.properties")
+        if (file.exists()) {
+            load(file.inputStream())
+        }
+    }
+
+    val BASE_URL: String = properties.getProperty("SUPABASE_URL") ?: ""
+    val API_KEY: String = properties.getProperty("SUPABASE_KEY") ?: ""
+    val BEARER_TOKEN: String = API_KEY
 }
