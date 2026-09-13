a25d44df00a55d3b0034e874b8c99e338c5e153f
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-13 19:25:09 +0300

    feat: implement shared Supabase HTTP client and networking config

---
+++ src/main/kotlin/data/remote/SupabaseHttpClient.kt
@@ -0,0 +1,51 @@
+package data.remote
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+object SupabaseHttpClient {
+    private val customJson = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(customJson)
+        }
+
+        defaultRequest {
+            url(SupabaseConfig.BASE_URL + "/")
+            header("apikey", SupabaseConfig.API_KEY)
+            header("Authorization", "Bearer ${SupabaseConfig.BEARER_TOKEN}")
+            contentType(ContentType.Application.Json)
+        }
+
+    }
+
+    suspend fun get(table: String): HttpResponse {
+        return client.get(table)
+    }
+
+    suspend inline fun <reified T> post(table: String, body: T): HttpResponse {
+        return client.post(table) {
+            setBody(body)
+        }
+    }
+
+    suspend inline fun <reified T> patch(table: String, id: String, body: T): HttpResponse {
+        return client.patch("$table?id=eq.$id") {
+            setBody(body)
+        }
+    }
+
+    suspend fun delete(table: String, id: String): HttpResponse {
+        return client.delete("$table?id=eq.$id")
+    }
+}
