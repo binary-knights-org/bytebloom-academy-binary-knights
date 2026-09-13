845f9fa4c1c70a30fbcabf391271d4d6051ab3c0
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-13 19:25:01 +0300

    build: add Ktor and Kotlinx serialization dependencies for Supabase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.4.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
+    kotlin("plugin.serialization") version "2.4.0"
 }
 
 group = "org.example"
@@ -12,6 +13,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    val ktorVersion = "2.3.12"
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
 
 kotlin {
