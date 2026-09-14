5b2de7438a2ea25ca2001846bc2196aae27bcb24
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-14 14:48:53 +0300

    fix: use serialname for WarehouseDto class

--- src/main/kotlin/data/remote/dto/WarehouseDto.kt
+++ src/main/kotlin/data/remote/dto/WarehouseDto.kt
@@ -1,12 +1,13 @@
 package data.remote.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class WarehouseDto (
-    val hubId: String,
-    val hubName: String,
-    val regionalZone: String,
+    @SerialName ("hub_id") val hubId: String,
+    @SerialName ("hub_name") val hubName: String,
+    @SerialName ("regional_zone") val regionalZone: String,
     val latitude: Double,
     val longitude: Double
 )
