769834dcd442d4387e46d0fcd58adcc27553757c
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-14 18:27:23 +0300

    refactor(config): migrate supabase credentials to environment variables

--- src/main/kotlin/data/remote/SupabaseConfig.kt
+++ src/main/kotlin/data/remote/SupabaseConfig.kt
@@ -1,8 +1,5 @@
 package data.remote
 
-import java.io.File
-import java.util.Properties
-
 object SupabaseConfig {
     val BASE_URL: String = System.getenv("SUPABASE_URL") ?: ""
     val API_KEY: String = System.getenv("SUPABASE_KEY") ?: ""
