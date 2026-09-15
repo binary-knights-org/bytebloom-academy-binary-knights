a561cebb8e4a82cda34e512435e52e9ecc19f632
Ibrahim Salha <ibrahim.salha19@gmail.com>
2026-09-15 23:22:59 +0300

    feat: implement domain CRUD use cases for routes [ST2]

---
+++ src/main/kotlin/domain/usecase/crud/CreateRouteUseCase.kt
@@ -0,0 +1,12 @@
+package domain.usecase.crud
+
+import domain.model.Route
+import domain.repository.RouteRepository
+
+class CreateRouteUseCase(
+    private val routeRepository: RouteRepository
+) {
+    suspend operator fun invoke(route: Route): Boolean {
+        return routeRepository.createRoute(route)
+    }
+}
