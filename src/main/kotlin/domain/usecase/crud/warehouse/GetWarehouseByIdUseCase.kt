2e52db9c117605114c9094740b57ae4b9af66698
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-16 01:58:43 +0300

    feat: add GetWarehouseByIdUseCase in crud.warehouse

---
+++ src/main/kotlin/domain/usecase/crud/warehouse/GetWarehouseByIdUseCase.kt
@@ -0,0 +1,12 @@
+package domain.usecase.crud.warehouse
+
+import domain.model.Warehouse
+import domain.repository.WarehouseRepository
+
+class GetWarehouseByIdUseCase(
+    private val warehouseRepository: WarehouseRepository
+) {
+    suspend operator fun invoke(warehouseId:String): Warehouse? {
+        return warehouseRepository.getWarehouseById(warehouseId)
+    }
+}
