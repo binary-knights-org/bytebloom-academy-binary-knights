4735b160118537158627abed3da23d1efdd3e959
Fatima-k-AbuAzab <fk42117638@gmail.com>
2026-09-15 19:28:43 +0300

    feat: add supabase http client factory

---
+++ src/main/kotlin/data/remote/SupabaseHttpClientFactory.kt
@@ -0,0 +1,36 @@
+package data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object SupabaseHttpClientFactory {
+
+    fun create(): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+
+            defaultRequest {
+                url(SupabaseConfig.BASE_URL + "/")
+                header("apikey", SupabaseConfig.API_KEY)
+                header(
+                    "Authorization",
+                    "Bearer ${SupabaseConfig.BEARER_TOKEN}"
+                )
+                contentType(ContentType.Application.Json)
+            }
+        }
+    }
+}
