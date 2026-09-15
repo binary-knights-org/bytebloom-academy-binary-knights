dbb247132b78ad231c5d7d439a8f43e24a15bed6
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-16 01:49:29 +0300

    refactor: move Route CRUD use cases into domain.usecase.crud.route

--- src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
@@ -1,12 +1,12 @@
-package domain.usecase.crud
+package domain.usecase.crud.route
 
 import domain.model.Route
 import domain.repository.RouteRepository
 
-class CreateRouteUseCase(
+class UpdateRouteUseCase(
     private val routeRepository: RouteRepository
 ) {
     suspend operator fun invoke(route: Route): Boolean {
-        return routeRepository.createRoute(route)
+        return routeRepository.updateRoute(route)
     }
 }
