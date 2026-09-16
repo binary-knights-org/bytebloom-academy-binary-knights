4370f1dfff4550df1e232b2f37714091cb4d5be8
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:17:49 +0300

    feat(usecase): validate warehouse fields before create

--- src/main/kotlin/domain/usecase/crud/warehouse/CreateWarehouseUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/warehouse/CreateWarehouseUseCase.kt
@@ -2,11 +2,27 @@ package domain.usecase.crud.warehouse
 
 import domain.model.Warehouse
 import domain.repository.WarehouseRepository
+import domain.validator.WarehouseCreateFields
+import domain.validator.WarehouseValidator
+import domain.validator.ValidationResult
 
 class CreateWarehouseUseCase(
     private val warehouseRepository: WarehouseRepository
 ) {
-    suspend operator fun invoke ( warehouse: Warehouse): Boolean {
+    suspend operator fun invoke(warehouse: Warehouse): Boolean {
+        val fields = WarehouseCreateFields(
+            hubId = warehouse.id,
+            hubName = warehouse.name,
+            regionalZone = warehouse.regionalZone,
+            latitude = warehouse.latitude,
+            longitude = warehouse.longitude
+        )
+
+        val validationResult = WarehouseValidator.validateForCreate(fields)
+        if (validationResult is ValidationResult.Failure) {
+            return false
+        }
+
         return warehouseRepository.createWarehouse(warehouse)
     }
 }
