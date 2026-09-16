f753a6858372ec64778c6d95b8ac474f3fbe8d27
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:22:27 +0300

    feat(usecase): validate package fields before create

--- src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
@@ -2,11 +2,27 @@ package domain.usecase.crud.packages
 
 import domain.model.Package
 import domain.repository.PackageRepository
+import domain.validator.PackageCreateFields
+import domain.validator.PackageValidator
+import domain.validator.ValidationResult
 
 class CreatePackageUseCase(
     private val packageRepository: PackageRepository
 ) {
-    suspend operator fun invoke(pkg: Package): Boolean{
+    suspend operator fun invoke(pkg: Package): Boolean {
+        val fields = PackageCreateFields(
+            packageId = pkg.id,
+            weight = pkg.weight,
+            originHubId = pkg.originHub.id,
+            destinationHubId = pkg.destinationHub.id,
+            priority = pkg.priority
+        )
+
+        val validationResult = PackageValidator.validateForCreate(fields)
+        if (validationResult is ValidationResult.Failure) {
+            return false
+        }
+
         return packageRepository.createPackage(pkg)
     }
 }
