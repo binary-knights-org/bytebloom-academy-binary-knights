e357cf3c52a7b5843975c6020385569e5b37b42f
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:18:17 +0300

    feat(usecase): validate warehouse id before get-by-id

--- src/main/kotlin/domain/usecase/crud/warehouse/GetWarehouseByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/warehouse/GetWarehouseByIdUseCase.kt
@@ -2,11 +2,18 @@ package domain.usecase.crud.warehouse
 
 import domain.model.Warehouse
 import domain.repository.WarehouseRepository
+import domain.validator.ValidationResult
+import domain.validator.WarehouseValidator
 
 class GetWarehouseByIdUseCase(
     private val warehouseRepository: WarehouseRepository
 ) {
-    suspend operator fun invoke(warehouseId:String): Warehouse? {
+    suspend operator fun invoke(warehouseId: String): Warehouse? {
+        val validationResult = WarehouseValidator.validateId(warehouseId)
+        if (validationResult is ValidationResult.Failure) {
+            return null
+        }
+
         return warehouseRepository.getWarehouseById(warehouseId)
     }
 }
