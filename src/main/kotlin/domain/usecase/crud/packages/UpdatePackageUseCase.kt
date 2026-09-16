bb4ce1bfdf7564faab00067637fe29785c11f183
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:23:00 +0300

    feat(usecase): validate package id and fields before update

--- src/main/kotlin/domain/usecase/crud/packages/UpdatePackageUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/UpdatePackageUseCase.kt
@@ -2,11 +2,30 @@ package domain.usecase.crud.packages
 
 import domain.model.Package
 import domain.repository.PackageRepository
+import domain.validator.PackageUpdateFields
+import domain.validator.PackageValidator
+import domain.validator.ValidationResult
 
 class UpdatePackageUseCase(
     private val packageRepository: PackageRepository
 ) {
     suspend operator fun invoke(pkg: Package): Boolean {
+        val idValidation = PackageValidator.validateId(pkg.id)
+        if (idValidation is ValidationResult.Failure) {
+            return false
+        }
+
+        val fields = PackageUpdateFields(
+            weight = pkg.weight,
+            destinationHubId = pkg.destinationHub.id,
+            priority = pkg.priority
+        )
+
+        val fieldsValidation = PackageValidator.validateForUpdate(fields)
+        if (fieldsValidation is ValidationResult.Failure) {
+            return false
+        }
+
         return packageRepository.updatePackage(pkg)
     }
 }
