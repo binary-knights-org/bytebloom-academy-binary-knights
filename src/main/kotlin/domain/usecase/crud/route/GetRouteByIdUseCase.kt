d39d4d388d28441a227b8435c99b98212cbef4ed
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:21:32 +0300

    feat(usecase): validate route id before get-by-id

--- src/main/kotlin/domain/usecase/crud/route/GetRouteByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/GetRouteByIdUseCase.kt
@@ -2,11 +2,18 @@ package domain.usecase.crud.route
 
 import domain.model.Route
 import domain.repository.RouteRepository
+import domain.validator.RouteValidator
+import domain.validator.ValidationResult
 
 class GetRouteByIdUseCase(
     private val routeRepository: RouteRepository
 ) {
     suspend operator fun invoke(id: String): Route? {
+        val validationResult = RouteValidator.validateId(id)
+        if (validationResult is ValidationResult.Failure) {
+            return null
+        }
+
         return routeRepository.getRouteById(id)
     }
 }
