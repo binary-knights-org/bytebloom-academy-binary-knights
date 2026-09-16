4d94ee231de0f1d541ecccaeef9cdcdadba7065c
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:19:58 +0300

    feat(usecase): validate vehicle id before get-by-id

--- src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
@@ -2,8 +2,16 @@ package domain.usecase.crud.vehicle
 
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
+import domain.validator.ValidationResult
+import domain.validator.VehicleValidator
 
 class GetVehicleByIdUseCase(private val vehicleRepository: VehicleRepository) {
     suspend operator fun invoke(id: String): Vehicle? {
+        val validationResult = VehicleValidator.validateId(id)
+        if (validationResult is ValidationResult.Failure) {
+            return null
+        }
+
         return vehicleRepository.getByID(id)
-    }}
+    }
+}
