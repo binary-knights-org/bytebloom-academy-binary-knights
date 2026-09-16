036b1cedfe629ac259136bae20a19f438ec1d44d
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:21:53 +0300

    feat(usecase): validate route id and fields before update

--- src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
@@ -2,11 +2,30 @@ package domain.usecase.crud.route
 
 import domain.model.Route
 import domain.repository.RouteRepository
+import domain.validator.RouteUpdateFields
+import domain.validator.RouteValidator
+import domain.validator.ValidationResult
 
 class UpdateRouteUseCase(
     private val routeRepository: RouteRepository
 ) {
     suspend operator fun invoke(route: Route): Boolean {
+        val idValidation = RouteValidator.validateId(route.id)
+        if (idValidation is ValidationResult.Failure) {
+            return false
+        }
+
+        val fields = RouteUpdateFields(
+            distanceKm = route.distanceKm,
+            typicalDelayMin = route.typicalDelayMin,
+            destinationHubId = route.destinationHub.id
+        )
+
+        val fieldsValidation = RouteValidator.validateForUpdate(fields)
+        if (fieldsValidation is ValidationResult.Failure) {
+            return false
+        }
+
         return routeRepository.updateRoute(route)
     }
 }
