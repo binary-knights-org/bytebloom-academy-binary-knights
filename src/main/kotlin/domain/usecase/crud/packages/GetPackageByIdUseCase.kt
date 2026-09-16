c1229dfca8f4314a569b9ed57d7c184b6497de75
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:22:43 +0300

    feat(usecase): validate package id before get-by-id

--- src/main/kotlin/domain/usecase/crud/packages/GetPackageByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/GetPackageByIdUseCase.kt
@@ -2,11 +2,18 @@ package domain.usecase.crud.packages
 
 import domain.model.Package
 import domain.repository.PackageRepository
+import domain.validator.PackageValidator
+import domain.validator.ValidationResult
 
 class GetPackageByIdUseCase(
     private val packageRepository: PackageRepository
 ) {
     suspend operator fun invoke(id: String): Package? {
+        val validationResult = PackageValidator.validateId(id)
+        if (validationResult is ValidationResult.Failure) {
+            return null
+        }
+
         return packageRepository.getPackageById(id)
     }
 }
