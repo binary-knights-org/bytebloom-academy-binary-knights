92c24bd722a6bf81f4d2c2b54d5f3746190f5a14
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:19:35 +0300

    feat(usecase): validate vehicle fields before create

--- src/main/kotlin/domain/usecase/crud/vehicle/CreateVehicleUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/CreateVehicleUseCase.kt
@@ -2,9 +2,24 @@ package domain.usecase.crud.vehicle
 
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
+import domain.validator.ValidationResult
+import domain.validator.VehicleCreateFields
+import domain.validator.VehicleValidator
 
 class CreateVehicleUseCase(private val vehicleRepository: VehicleRepository) {
-    suspend operator fun invoke(vehicle: Vehicle): Vehicle {
+    suspend operator fun invoke(vehicle: Vehicle): Vehicle? {
+        val fields = VehicleCreateFields(
+            vehicleId = vehicle.id,
+            maxCapacityKg = vehicle.maxCapacityKg,
+            costPerKm = vehicle.costPerKm,
+            currentHubId = vehicle.currentHub.id
+        )
+
+        val validationResult = VehicleValidator.validateForCreate(fields)
+        if (validationResult is ValidationResult.Failure) {
+            return null
+        }
+
         return vehicleRepository.create(vehicle)
     }
 }
