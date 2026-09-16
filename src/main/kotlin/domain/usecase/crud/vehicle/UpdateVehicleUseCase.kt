b9c8eda2a7e5da593e4ccde3bc815db77aec707d
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:20:17 +0300

    feat(usecase): validate vehicle id and fields before update

--- src/main/kotlin/domain/usecase/crud/vehicle/UpdateVehicleUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/UpdateVehicleUseCase.kt
@@ -2,9 +2,28 @@ package domain.usecase.crud.vehicle
 
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
+import domain.validator.ValidationResult
+import domain.validator.VehicleUpdateFields
+import domain.validator.VehicleValidator
 
 class UpdateVehicleUseCase(private val vehicleRepository: VehicleRepository) {
-    suspend operator fun invoke(vehicle: Vehicle): Vehicle {
+    suspend operator fun invoke(vehicle: Vehicle): Vehicle? {
+        val idValidation = VehicleValidator.validateId(vehicle.id)
+        if (idValidation is ValidationResult.Failure) {
+            return null
+        }
+
+        val fields = VehicleUpdateFields(
+            maxCapacityKg = vehicle.maxCapacityKg,
+            costPerKm = vehicle.costPerKm,
+            currentHubId = vehicle.currentHub.id
+        )
+
+        val fieldsValidation = VehicleValidator.validateForUpdate(fields)
+        if (fieldsValidation is ValidationResult.Failure) {
+            return null
+        }
+
         return vehicleRepository.update(vehicle)
     }
 }
