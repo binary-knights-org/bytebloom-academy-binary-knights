793a50c16253ce8d244dd62ef1b7494ea5e9e6e2
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:18:54 +0300

    feat(usecase): validate warehouse id and fields before update

--- src/main/kotlin/domain/usecase/crud/warehouse/UpdateWarehouseUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/warehouse/UpdateWarehouseUseCase.kt
@@ -2,11 +2,31 @@ package domain.usecase.crud.warehouse
 
 import domain.model.Warehouse
 import domain.repository.WarehouseRepository
+import domain.validator.ValidationResult
+import domain.validator.WarehouseUpdateFields
+import domain.validator.WarehouseValidator
 
 class UpdateWarehouseUseCase(
     private val warehouseRepository: WarehouseRepository
 ) {
     suspend operator fun invoke(warehouse: Warehouse): Boolean {
-       return warehouseRepository.updateWarehouse(warehouse)
+        val idValidation = WarehouseValidator.validateId(warehouse.id)
+        if (idValidation is ValidationResult.Failure) {
+            return false
+        }
+
+        val fields = WarehouseUpdateFields(
+            hubName = warehouse.name,
+            regionalZone = warehouse.regionalZone,
+            latitude = warehouse.latitude,
+            longitude = warehouse.longitude
+        )
+
+        val fieldsValidation = WarehouseValidator.validateForUpdate(fields)
+        if (fieldsValidation is ValidationResult.Failure) {
+            return false
+        }
+
+        return warehouseRepository.updateWarehouse(warehouse)
     }
 }
