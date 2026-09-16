677399bc93696b62d253b2ed029526144d5f2522
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-17 01:21:02 +0300

    eat(usecase): validate route fields before create

--- src/main/kotlin/domain/usecase/crud/route/CreateRouteUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/CreateRouteUseCase.kt
@@ -2,11 +2,27 @@ package domain.usecase.crud.route
 
 import domain.model.Route
 import domain.repository.RouteRepository
+import domain.validator.RouteCreateFields
+import domain.validator.RouteValidator
+import domain.validator.ValidationResult
 
 class CreateRouteUseCase(
     private val routeRepository: RouteRepository
 ) {
     suspend operator fun invoke(route: Route): Boolean {
+        val fields = RouteCreateFields(
+            routeId = route.id,
+            distanceKm = route.distanceKm,
+            typicalDelayMin = route.typicalDelayMin,
+            originHubId = route.originHub.id,
+            destinationHubId = route.destinationHub.id
+        )
+
+        val validationResult = RouteValidator.validateForCreate(fields)
+        if (validationResult is ValidationResult.Failure) {
+            return false
+        }
+
         return routeRepository.createRoute(route)
     }
 }
