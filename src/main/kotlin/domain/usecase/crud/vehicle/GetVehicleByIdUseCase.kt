b5a834cfd68e2539ccfdf50d8d2b04b0291273a7
Tariq-Zeyad <146614525+Tariq-Zeyad@users.noreply.github.com>
2026-09-16 14:14:57 +0300

    feat: implement vehicle CRUD data source

--- src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
@@ -1,12 +1,9 @@
 package domain.usecase.crud.vehicle
 
-import domain.exception.VehicleException
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
 
 class GetVehicleByIdUseCase(private val vehicleRepository: VehicleRepository) {
-suspend  operator fun invoke(id: String): Vehicle {
+    suspend operator fun invoke(id: String): Vehicle? {
         return vehicleRepository.getByID(id)
-            ?: throw VehicleException.NotFound(id)
-    }
-}
+    }}
