3e4865ca7e1f79148d082c7628fc8d0cb96dfee3
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-17 19:17:15 +0300

    refactor(usecase): integrate CreatePackageValidator and CreatePackageInput

--- src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
@@ -1,24 +1,29 @@
 package domain.usecase.crud.packages
 
 import domain.model.Package
+import domain.model.input.CreatePackageInput
 import domain.repository.PackageRepository
-import domain.validator.PackageCreateFields
-import domain.validator.PackageValidator
+import domain.validator.FieldViolation
 import domain.validator.ValidationResult
+import domain.validator.packages.CreatePackageValidator
 
 class CreatePackageUseCase(
-    private val packageRepository: PackageRepository
+    private val packageRepository: PackageRepository,
+    private val validator: CreatePackageValidator
 ) {
-    suspend operator fun invoke(pkg: Package): Boolean {
-        val fields = PackageCreateFields(
-            packageId = pkg.id,
-            weight = pkg.weight,
-            originHubId = pkg.originHub.id,
-            destinationHubId = pkg.destinationHub.id,
-            priority = pkg.priority
+    suspend operator fun invoke(input: CreatePackageInput): ValidationResult {
+        val validation = validator.validate(input)
+        if (validation is ValidationResult.Invalid) return validation
+
+        val pkg = Package.create(
+            id = input.id,
+            weight = input.weight,
+            priority = input.priority,
+            originHub = input.originHub,
+            destinationHub = input.destinationHub
         )
 
-        val isValid = PackageValidator.validateForCreate(fields) is ValidationResult.Success
-        return isValid && packageRepository.create(pkg)
+        return if (packageRepository.create(pkg)) ValidationResult.Valid
+        else ValidationResult.Invalid(listOf(FieldViolation("database", "Failed to create package.")))
     }
 }
