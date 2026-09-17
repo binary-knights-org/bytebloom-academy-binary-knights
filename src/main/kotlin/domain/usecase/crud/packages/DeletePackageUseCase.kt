15ec5bbfeb34502d2dc3a15c42d68f3128b93290
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-17 19:17:30 +0300

    refactor(usecase): add PackageIdValidator execution to DeletePackageUseCase

--- src/main/kotlin/domain/usecase/crud/packages/DeletePackageUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/DeletePackageUseCase.kt
@@ -1,14 +1,27 @@
 package domain.usecase.crud.packages
 
 import domain.repository.PackageRepository
-import domain.validator.PackageValidator
+import domain.validator.FieldViolation
 import domain.validator.ValidationResult
+import domain.validator.packages.PackageIdValidator
 
 class DeletePackageUseCase(
-    private val packageRepository: PackageRepository
+    private val packageRepository: PackageRepository,
+    private val idValidator: PackageIdValidator
 ) {
-    suspend operator fun invoke(id: String): Boolean {
-        val isValid = PackageValidator.validateId(id) is ValidationResult.Success
-        return isValid && packageRepository.delete(id)
+    suspend operator fun invoke(id: String): ValidationResult {
+        val validationResult = idValidator.validate(id)
+        if (validationResult is ValidationResult.Invalid) {
+            return validationResult
+        }
+
+        val isDeleted = packageRepository.delete(id)
+        return if (isDeleted) {
+            ValidationResult.Valid
+        } else {
+            ValidationResult.Invalid(
+                listOf(FieldViolation("database", "Failed to delete package from database."))
+            )
+        }
     }
 }
