ca1b3f0e08e7f948072ccd2fedca51f1fd4bb79a
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-19 02:28:55 +0300

    refactor(vehicle): validate ID on get via VehicleIdValidator

--- src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
@@ -1,16 +1,20 @@
 package domain.usecase.crud.vehicle
 
+import domain.exception.EntityValidationException
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
 import domain.validator.ValidationResult
-import domain.validator.VehicleValidator
+import domain.validator.vehicle.VehicleIdValidator
 
 class GetVehicleByIdUseCase(
-    private val vehicleRepository: VehicleRepository
+    private val vehicleRepository: VehicleRepository,
+    private val idValidator: VehicleIdValidator
 ) {
     suspend operator fun invoke(id: String): Vehicle? {
-        val isVaild = VehicleValidator.validateId(id) is ValidationResult.Success
-        return  if (isVaild) vehicleRepository.getById(id) else null
+        if (idValidator.validate(id) is ValidationResult.Failure) {
+            throw EntityValidationException("Cannot get vehicle: invalid vehicle ID.")
+        }
+
+        return vehicleRepository.getById(id)
     }
 }
-
