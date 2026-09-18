09d8058489878af94dac00953ad0e3ab17df2912
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-18 20:41:24 +0300

    feat(domain): introduce UpdateRouteInput DTO for partial route modifications

---
+++ src/main/kotlin/domain/model/input/UpdateRouteInput.kt
@@ -0,0 +1,11 @@
+package domain.model.input
+
+import domain.model.Warehouse
+
+data class UpdateRouteInput (
+    val id: String,
+    val distanceKm: Double? = null,
+    val typicalDelayMin: Int? = null,
+    val originHub: Warehouse? = null,
+    val destinationHub: Warehouse? = null
+)
