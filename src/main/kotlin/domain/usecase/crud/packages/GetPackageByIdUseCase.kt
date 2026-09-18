6b0c6cb16a91c24c5e28910c89b1df54eb972b6c
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-18 20:40:43 +0300

    refactor(usecase): streamline GetPackageByIdUseCase execution

--- src/main/kotlin/domain/usecase/crud/packages/GetPackageByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/GetPackageByIdUseCase.kt
@@ -1,33 +1,24 @@
 package domain.usecase.crud.packages
 
+import domain.exception.EntityValidationException
 import domain.model.Package
 import domain.repository.PackageRepository
-import domain.validator.FieldViolation
 import domain.validator.ValidationResult
 import domain.validator.packages.PackageIdValidator
-
-sealed interface GetPackageResult {
-    data class Success(val pkg: Package) : GetPackageResult
-    data class Failure(val violations: List<FieldViolation>) : GetPackageResult
-}
 
 class GetPackageByIdUseCase(
     private val packageRepository: PackageRepository,
     private val idValidator: PackageIdValidator
 ) {
-    suspend operator fun invoke(id: String): GetPackageResult {
-        val validationResult = idValidator.validate(id)
-        if (validationResult is ValidationResult.Invalid) {
-            return GetPackageResult.Failure(validationResult.violations)
-        }
+    suspend operator fun invoke(id: String): Package? {
+        val validation = idValidator.validate(id)
 
-        val pkg = packageRepository.getById(id)
-        return if (pkg != null) {
-            GetPackageResult.Success(pkg)
-        } else {
-            GetPackageResult.Failure(
-                listOf(FieldViolation("id", "Package with ID '$id' was not found."))
+        if (validation is ValidationResult.Failure) {
+            throw EntityValidationException(
+                "Cannot get package: invalid package ID."
             )
         }
+
+        return packageRepository.getById(id)
     }
 }
