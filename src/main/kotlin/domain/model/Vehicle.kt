24131f777d7320c17677adaa4e22634794044f90
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-19 01:00:42 +0300

    refactor: enforce enterprise rules in Vehicle via private constructor + factory

--- src/main/kotlin/domain/model/Vehicle.kt
+++ src/main/kotlin/domain/model/Vehicle.kt
@@ -1,6 +1,9 @@
 package domain.model
 
-data class Vehicle(
+import domain.exception.EntityValidationException
+import java.util.UUID
+
+data class Vehicle private constructor(
     val id: String,
     val maxCapacityKg: Double,
     val costPerKm: Double,
@@ -25,4 +28,67 @@ data class Vehicle(
         mutableLoadedCargo.addAll(packages)
     }
 
+    companion object {
+
+        const val ID_PREFIX = "TRK-"
+        const val MIN_CAPACITY_KG = 0.0
+        const val MIN_COST_PER_KM = 0.0
+
+
+        fun isValidId(id: String) : Boolean {
+            if (id.isBlank()) return false
+
+            val hasValidPrefix = id.startsWith(ID_PREFIX)
+
+            val isUUID = runCatching {
+                UUID.fromString(id)
+            }.isSuccess
+
+             return hasValidPrefix || isUUID
+        }
+
+        fun isValidCapacity(maxCapacityKg: Double): Boolean {
+            if (maxCapacityKg > MIN_CAPACITY_KG )
+                return true
+            return false
+        }
+
+        fun isValidCostPerKm( costPerKm: Double): Boolean {
+            if ( costPerKm > MIN_COST_PER_KM )
+                return true
+            return false
+        }
+
+        fun create(
+            id: String,
+            maxCapacityKg: Double,
+            costPerKm: Double,
+            currentHub: Warehouse
+        ) : Vehicle {
+
+            val validationError = when {
+                !isValidId(id) ->
+                    "Invalid ID format ,,, Must start with $ID_PREFIX or be a valid UUID."
+                !isValidCapacity(maxCapacityKg) ->
+                    "Max capacity must be greater than $MIN_CAPACITY_KG."
+                !isValidCostPerKm(costPerKm) ->
+                    "Cost per km must be greater than $MIN_COST_PER_KM "
+
+                else -> null
+            }
+
+            if(validationError != null){
+                throw EntityValidationException(validationError)
+            }
+
+            return Vehicle(
+                id = id,
+                maxCapacityKg = maxCapacityKg,
+                costPerKm = costPerKm,
+                currentHub = currentHub
+            )
+
+        }
+    }
 }
+
