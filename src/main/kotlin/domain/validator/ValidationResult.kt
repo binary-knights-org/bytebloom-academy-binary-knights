3915751048311cf35d359c5cde641272638e93ee
Fatima-k-AbuAzab <fk42117638@gmail.com>
2026-09-18 15:32:52 +0300

    refactor: update validation result structure

--- src/main/kotlin/domain/validator/ValidationResult.kt
+++ src/main/kotlin/domain/validator/ValidationResult.kt
@@ -1,15 +1,11 @@
 package domain.validator
 
 sealed class ValidationResult {
-
-    data object Valid : ValidationResult()
-
-    data class Invalid(
-        val violations: List<FieldViolation>
-    ) : ValidationResult()
+    object Success : ValidationResult()
+    data class Failure(val errors: List<FieldError>) : ValidationResult()
 }
 
-data class FieldViolation(
-    val field: String,
+data class FieldError(
+    val fieldName: String,
     val message: String
 )
