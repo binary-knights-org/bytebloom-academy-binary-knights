0098aa6d758cf08d5063f15f7ef729465fbd59c5
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-18 20:41:30 +0300

    refactor(usecase): integrate validation and merging logic into UpdateRouteUseCase

--- src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/UpdateRouteUseCase.kt
@@ -1,7 +1,9 @@
 package domain.usecase.crud.route
 
 import domain.exception.EntityValidationException
+import domain.model.Package
 import domain.model.Route
+import domain.model.input.UpdateRouteInput
 import domain.repository.RouteRepository
 import domain.validator.ValidationResult
 import domain.validator.routes.RouteIdValidator
@@ -12,27 +14,31 @@ class UpdateRouteUseCase(
     private val idValidator: RouteIdValidator,
     private val validator: UpdateRouteValidator
 ) {
-    suspend operator fun invoke(route: Route): Boolean {
-        val idValidation = idValidator.validate(route.id)
-
+    suspend operator fun invoke(input: UpdateRouteInput): Boolean {
+        val idValidation = idValidator.validate(input.id)
         if (idValidation is ValidationResult.Failure) {
             throw EntityValidationException(
                 "Cannot update route: invalid route ID."
             )
         }
 
-        val fieldValidation = validator.validate(
-            distanceKm = route.distanceKm,
-            typicalDelayMin = route.typicalDelayMin,
-            destinationHub = route.destinationHub
-        )
-
+        val fieldValidation = validator.validate(input)
         if (fieldValidation is ValidationResult.Failure) {
-            throw EntityValidationException(
-                "Cannot update route: invalid route data."
-            )
+            throw EntityValidationException("Cannot update package: invalid package data.")
         }
 
-        return routeRepository.update(route)
+        val existingPkg = routeRepository.getById(input.id)
+            ?: throw EntityValidationException("Route with ID '${input.id}' was not found.")
+
+
+        val updatedPkg = Route.create(
+            id = existingPkg.id,
+            distanceKm = input.distanceKm ?: existingPkg.distanceKm,
+            typicalDelayMin = input.typicalDelayMin ?: existingPkg.typicalDelayMin,
+            originHub = input.originHub ?: existingPkg.originHub,
+            destinationHub = input.destinationHub ?: existingPkg.destinationHub
+        )
+
+        return routeRepository.update(updatedPkg)
     }
 }
