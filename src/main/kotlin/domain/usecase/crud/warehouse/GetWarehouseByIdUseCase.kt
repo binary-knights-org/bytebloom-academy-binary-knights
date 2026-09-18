7eae7f201f31ca0bc2b0dbc58cf5651a5415d4de
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-19 02:27:56 +0300

    refactor(warehouse): validate ID on get via WarehouseIdValidator

--- src/main/kotlin/domain/usecase/crud/warehouse/GetWarehouseByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/warehouse/GetWarehouseByIdUseCase.kt
@@ -1,15 +1,20 @@
 package domain.usecase.crud.warehouse
 
+import domain.exception.EntityValidationException
 import domain.model.Warehouse
 import domain.repository.WarehouseRepository
 import domain.validator.ValidationResult
-import domain.validator.WarehouseValidator
+import domain.validator.warehouse.WarehouseIdValidator
 
 class GetWarehouseByIdUseCase(
-    private val warehouseRepository: WarehouseRepository
+    private val warehouseRepository: WarehouseRepository,
+    private val idValidator: WarehouseIdValidator
 ) {
-    suspend operator fun invoke(warehouseId: String): Warehouse? {
-        val isValid = WarehouseValidator.validateId(warehouseId) is ValidationResult.Success
-        return if (isValid) warehouseRepository.getById(warehouseId) else null
+    suspend operator fun invoke(id: String): Warehouse? {
+        if (idValidator.validate(id) is ValidationResult.Failure) {
+            throw EntityValidationException("Cannot get warehouse: invalid warehouse ID.")
+        }
+
+        return warehouseRepository.getById(id)
     }
 }
