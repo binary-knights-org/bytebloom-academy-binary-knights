865ec6f9af3009e57c427de005a4177acad0a17e
Kamal Ashour <ks.ashour@std.alaqsa.edu.ps>
2026-09-19 01:02:00 +0300

    feat: add UpdateVehicleValidator for partial update validation

---
+++ src/main/kotlin/domain/validator/vehicle/UpdateVehicleValidator.kt
@@ -0,0 +1,49 @@
+package domain.validator.vehicle
+
+import domain.model.Vehicle
+import domain.model.input.UpdateVehicleInput
+import domain.validator.FieldError
+import domain.validator.ValidationResult
+
+class UpdateVehicleValidator {
+
+    fun validate(
+        input: UpdateVehicleInput
+    ): ValidationResult {
+        val errors = mutableListOf<FieldError>()
+
+        if (input.currentHub == null && input.costPerKm == null &&  input.maxCapacityKg == null) {
+            errors.add(
+                FieldError(
+                    "update", "At least one field must be provided for update."
+                )
+            )
+        }
+
+        input.maxCapacityKg?.let {
+            if (!Vehicle.isValidCapacity(it)){
+                errors.add(
+                    FieldError(
+                        "maxCapacityKg", " Max Capacity Kg  must not be negative."
+                    )
+                )
+            }
+        }
+
+        input.costPerKm?.let {
+            if(!Vehicle.isValidCostPerKm(it)){
+                errors.add(
+                    FieldError(
+                        "costPerKm", " Cost Per Km  must not be negative."
+                    )
+                )
+            }
+        }
+
+        return if (errors.isEmpty()){
+            ValidationResult.Success
+        } else {
+            ValidationResult.Failure(errors)
+        }
+    }
+}
