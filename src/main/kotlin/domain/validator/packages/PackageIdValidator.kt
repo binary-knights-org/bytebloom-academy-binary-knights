e446d2ea3aeb54bf0f2176517373aff3b68890b1
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-18 20:41:04 +0300

    refactor(validator): refine PackageIdValidator error messages

--- src/main/kotlin/domain/validator/packages/PackageIdValidator.kt
+++ src/main/kotlin/domain/validator/packages/PackageIdValidator.kt
@@ -1,16 +1,22 @@
 package domain.validator.packages
 
 import domain.model.Package
-import domain.validator.FieldViolation
+import domain.validator.FieldError
 import domain.validator.ValidationResult
 
 class PackageIdValidator {
+
     fun validate(id: String): ValidationResult {
         return if (Package.isValidId(id)) {
-            ValidationResult.Valid
+            ValidationResult.Success
         } else {
-            ValidationResult.Invalid(
-                listOf(FieldViolation("id", "Package ID must start with '${Package.ID_PREFIX}' or be a valid UUID."))
+            ValidationResult.Failure(
+                listOf(
+                    FieldError(
+                        "id",
+                        "Package ID must start with '${Package.ID_PREFIX}' or be a valid UUID."
+                    )
+                )
             )
         }
     }
