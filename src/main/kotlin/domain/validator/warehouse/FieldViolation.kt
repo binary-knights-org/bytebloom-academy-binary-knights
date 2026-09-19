c6fe99fc75aab95f4c7dcc70a36751221ad4ecef
Ibrahim Salha <ibrahim.salha19@gmail.com>
2026-09-19 23:08:30 +0300

    feat(exception): define domain exception hierarchy and validation result types

---
+++ src/main/kotlin/domain/validator/warehouse/FieldViolation.kt
@@ -0,0 +1,5 @@
+package domain.validator.warehouse
+
+typealias FieldViolation = domain.validator.FieldViolation
+typealias ValidationWarehouseResult = domain.validator.ValidationWarehouseResult
+typealias ValidationResultBuilder = domain.validator.ValidationResultBuilder
