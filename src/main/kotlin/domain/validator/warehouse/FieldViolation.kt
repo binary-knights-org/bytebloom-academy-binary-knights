1fa8b04c28fce8a9ba969c934dcb7831d235ddda
Ibrahim Salha <ibrahim.salha19@gmail.com>
2026-09-19 23:09:30 +0300

    docs: document Strategy A architectural error strategy in README.md

--- src/main/kotlin/domain/validator/warehouse/FieldViolation.kt
+++
@@ -1,5 +0,0 @@
-package domain.validator.warehouse
-
-typealias FieldViolation = domain.validator.FieldViolation
-typealias ValidationWarehouseResult = domain.validator.ValidationWarehouseResult
-typealias ValidationResultBuilder = domain.validator.ValidationResultBuilder
