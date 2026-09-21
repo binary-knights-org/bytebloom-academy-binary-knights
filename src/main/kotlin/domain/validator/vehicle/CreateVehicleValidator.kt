3f6a352a44a745a53ff24b2cfa16602c27123686
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:51:40 +0300

    refactor(validator): update create vehicle validation

--- src/main/kotlin/domain/validator/vehicle/CreateVehicleValidator.kt
+++ src/main/kotlin/domain/validator/vehicle/CreateVehicleValidator.kt
@@ -1,36 +1,24 @@
 package domain.validator.vehicle
 
 import domain.model.input.CreateVehicleInput
-import domain.validator.FieldViolation
 import domain.validator.ValidationResult
-import domain.validator.toValidationResult
 
-private const val MIN_CAPACITY_KG = 0.0
-private const val MIN_COST_PER_KM = 0.0
-
-class CreateVehicleValidator
- {
-    fun validate(input: CreateVehicleInput): ValidationResult {
-        val violations = mutableListOf<FieldViolation>()
-
-
-        if (input.maxCapacityKg <= MIN_CAPACITY_KG) {
-            violations.add(
-                FieldViolation(
-                    CreateVehicleInput::maxCapacityKg.name,
-                    "Max capacity must be greater than $MIN_CAPACITY_KG."
-                )
-            )
-        }
-        if (input.costPerKm <= MIN_COST_PER_KM) {
-            violations.add(
-                FieldViolation(
-                    CreateVehicleInput::costPerKm.name,
-                    "Cost per km must be greater than $MIN_COST_PER_KM."
-                )
-            )
+class CreateVehicleValidator {
+    fun validate(input: CreateVehicleInput): ValidationResult<VehicleValidationError> {
+        val violations = buildList {
+            if (input.maxCapacityKg <= MIN_CAPACITY_KG) {
+                add(VehicleValidationError.InvalidCapacity)
+            }
+            if (input.costPerKm <= MIN_COST_PER_KM) {
+                add(VehicleValidationError.InvalidCost)
+            }
         }
 
-        return violations.toValidationResult()
+        return if (violations.isEmpty()) ValidationResult.Valid else ValidationResult.Invalid(violations)
+    }
+
+    companion object{
+        const val MIN_CAPACITY_KG = 0.0
+        const val MIN_COST_PER_KM = 0.0
     }
 }
