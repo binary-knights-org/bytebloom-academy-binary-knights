b5005db262d731d75a61ce11ce3e1a4540b146eb
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:51:25 +0300

    refactor(validator): update create route validation

--- src/main/kotlin/domain/validator/routes/CreateRouteValidator.kt
+++ src/main/kotlin/domain/validator/routes/CreateRouteValidator.kt
@@ -1,44 +1,26 @@
 package domain.validator.routes
 
 import domain.model.input.CreateRouteInput
-import domain.validator.FieldViolation
 import domain.validator.ValidationResult
-import domain.validator.toValidationResult
 
-private const val MIN_DISTANCE_KM = 0.0
-private const val MIN_DELAY_MIN = 0
-
-class CreateRouteValidator
-{
-    fun validate(input: CreateRouteInput): ValidationResult {
-        val violations = mutableListOf<FieldViolation>()
-
-        if (input.distanceKm < MIN_DISTANCE_KM)
-           {  violations.add(
-               FieldViolation(
-                   CreateRouteInput::distanceKm.name,
-                   "Distance must be greater than $MIN_DISTANCE_KM."
-               )
-           )
+class CreateRouteValidator {
+    fun validate(input: CreateRouteInput): ValidationResult<RouteValidationError> {
+        val violations = buildList {
+            if (input.distanceKm <= MIN_DISTANCE_KM) {
+                add(RouteValidationError.InvalidDistance)
+            }
+            if (input.typicalDelayMin < MIN_DELAY_MIN) {
+                add(RouteValidationError.NegativeDelay)
+            }
+            if (input.originHub.id == input.destinationHub.id) {
+                add(RouteValidationError.SameOriginAndDestination)
+            }
         }
 
-        if (input.typicalDelayMin <= MIN_DELAY_MIN)
-            {  violations.add(
-                FieldViolation(
-                    CreateRouteInput::typicalDelayMin.name,
-                    "Typical delay must be at least $MIN_DELAY_MIN."
-                )
-            )
-        }
-
-        if (input.originHub.id == input.destinationHub.id)
-            {  violations.add(
-                FieldViolation(
-                    CreateRouteInput::destinationHub.name,
-                    "Destination hub must be different from origin hub."
-                )
-            )
-        }
-        return violations.toValidationResult()
+        return if (violations.isEmpty()) ValidationResult.Valid else ValidationResult.Invalid(violations)
+    }
+    companion object{
+        const val MIN_DISTANCE_KM = 0.0
+        const val MIN_DELAY_MIN = 0
     }
 }
