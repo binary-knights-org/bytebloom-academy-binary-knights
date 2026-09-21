1ef20a75b173ccc0a9af5b16f1d56df2d869b5d8
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:51:02 +0300

    refactor(usecase): update create package use case

--- src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/packages/CreatePackageUseCase.kt
@@ -1,7 +1,11 @@
 package domain.usecase.crud.packages
 
+import domain.model.exception.EntityValidationException
+import domain.model.exception.OperationFailedException
+import data.exception.translateDataError
 import domain.model.Package
 import domain.model.input.CreatePackageInput
+import domain.model.toPriority
 import domain.repository.PackageRepository
 import domain.validator.ValidationResult
 import domain.validator.packages.CreatePackageValidator
@@ -10,20 +14,33 @@ class CreatePackageUseCase(
     private val packageRepository: PackageRepository,
     private val validator: CreatePackageValidator
 ) {
-    suspend operator fun invoke(input: CreatePackageInput): ValidationResult {
+    suspend operator fun invoke(input: CreatePackageInput): Result<Package> {
         val validation = validator.validate(input)
-        if (validation is ValidationResult.Invalid) return validation
+        if (validation is ValidationResult.Invalid)
+            return Result.failure(EntityValidationException(validation.violations))
 
-        val newPackage = Package(
-            id = input.id,
-            weight = input.weight,
-            priority = input.priority,
-            originHub = input.originHub,
-            destinationHub = input.destinationHub
+        return runCatching {
+            Package(
+                id = input.id,
+                weight = input.weight,
+                priority = input.priority.toPriority(),
+                originHub = input.originHub,
+                destinationHub = input.destinationHub
+            )
+        }.fold(
+            onSuccess = { pkg ->
+                runCatching { packageRepository.create(pkg) }.fold(
+                    onSuccess = { isCreated ->
+                        if (isCreated) {
+                            Result.success(pkg)
+                        } else {
+                            Result.failure(OperationFailedException())
+                        }
+                    },
+                    onFailure = { error -> Result.failure(translateDataError(error, "create", "package")) }
+                )
+            },
+            onFailure = { error -> Result.failure(error) }
         )
-
-        packageRepository.create(newPackage)
-
-        return ValidationResult.Valid
     }
 }
