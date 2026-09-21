6ef99216ed8c5e6cafd8a4cda7a0b32e3947a957
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:53:59 +0300

    refactor(usecase): update get vehicle by ID use case

--- src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/vehicle/GetVehicleByIdUseCase.kt
@@ -1,27 +1,24 @@
 package domain.usecase.crud.vehicle
 
-import domain.exception.DatabaseConflictException
-import domain.exception.ResourceNotFoundException
+import data.exception.translateDataError
+import domain.model.exception.ResourceNotFoundException
 import domain.model.Vehicle
 import domain.repository.VehicleRepository
 
 class GetVehicleByIdUseCase(
-    private val vehicleRepository: VehicleRepository,
+    private val vehicleRepository: VehicleRepository
 ) {
     suspend operator fun invoke(id: String): Result<Vehicle> {
-
         return runCatching { vehicleRepository.getById(id) }.fold(
             onSuccess = { vehicle ->
                 if (vehicle != null) {
                     Result.success(vehicle)
                 } else {
-                    Result.failure(ResourceNotFoundException("Vehicle with ID '$id' was not found."))
+                    Result.failure(ResourceNotFoundException())
                 }
             },
             onFailure = { error ->
-                Result.failure(
-                    DatabaseConflictException(
-                        "Failed to fetch vehicle with ID '$id': ${error.message}", error))
+                Result.failure(translateDataError(error, "fetch", "vehicle"))
             }
         )
     }
