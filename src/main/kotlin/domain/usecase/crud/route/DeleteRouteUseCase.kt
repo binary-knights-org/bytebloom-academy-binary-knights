b3228b9ba2378682b3fbb96be373ce516f04a027
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:52:49 +0300

    refactor(usecase): update delete route use case

--- src/main/kotlin/domain/usecase/crud/route/DeleteRouteUseCase.kt
+++ src/main/kotlin/domain/usecase/crud/route/DeleteRouteUseCase.kt
@@ -1,23 +1,23 @@
 package domain.usecase.crud.route
 
-import domain.exception.DatabaseConflictException
+import domain.model.exception.OperationFailedException
+import data.exception.translateDataError
 import domain.repository.RouteRepository
 
 class DeleteRouteUseCase(
-    private val routeRepository: RouteRepository,
+    private val routeRepository: RouteRepository
 ) {
     suspend operator fun invoke(id: String): Result<Unit> {
-
         return runCatching { routeRepository.delete(id) }.fold(
             onSuccess = { isDeleted ->
                 if (isDeleted) {
                     Result.success(Unit)
                 } else {
-                    Result.failure(DatabaseConflictException("Failed to delete route with ID '$id' from database."))
+                    Result.failure(OperationFailedException())
                 }
             },
             onFailure = { error ->
-                Result.failure(DatabaseConflictException("Failed to delete route: ${error.message}", error))
+                Result.failure(translateDataError(error, "delete", "route"))
             }
         )
     }
