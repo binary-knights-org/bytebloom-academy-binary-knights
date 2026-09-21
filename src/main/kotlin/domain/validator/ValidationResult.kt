c91303ac89bcb7663ece4f9e700b649862a2c574
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:56:26 +0300

    refactor(validator): update ValidationResult structure

--- src/main/kotlin/domain/validator/ValidationResult.kt
+++ src/main/kotlin/domain/validator/ValidationResult.kt
@@ -1,22 +1,10 @@
 package domain.validator
 
-data class FieldViolation(
-    val field: String,
-    val message: String
-)
+sealed interface ValidationResult<out E> {
 
-sealed interface ValidationResult {
-    data object Valid : ValidationResult
+    data object Valid : ValidationResult<Nothing>
 
-    data class Invalid(val violations: List<FieldViolation>) : ValidationResult {
-        constructor(field: String, message: String) : this(listOf(FieldViolation(field, message)))
-    }
-
-    val isValid: Boolean get() = this is Valid
-    val isInvalid: Boolean get() = this is Invalid
-
-    fun errorsOrNull(): List<FieldViolation>? = (this as? Invalid)?.violations
+    data class Invalid<E>(
+        val violations: List<E>
+    ) : ValidationResult<E>
 }
-
-fun List<FieldViolation>.toValidationResult(): ValidationResult =
-    if (isEmpty()) ValidationResult.Valid else ValidationResult.Invalid(this)
