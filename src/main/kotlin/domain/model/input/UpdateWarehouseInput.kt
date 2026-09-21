ba2aad99d655a340b1d316be82e6733511767a92
ahmed <am.farani@std.alaqsa.edu.ps>
2026-09-21 16:56:07 +0300

    refactor(model): update UpdateWarehouseInput model

--- src/main/kotlin/domain/model/input/UpdateWarehouseInput.kt
+++ src/main/kotlin/domain/model/input/UpdateWarehouseInput.kt
@@ -1,13 +1,15 @@
 package domain.model.input
 
-import java.util.UUID
-
-private const val WAREHOUSE_ID_PREFIX = "WH-"
+import kotlin.uuid.Uuid
 
 data class UpdateWarehouseInput (
-    val id: String= "$WAREHOUSE_ID_PREFIX${UUID.randomUUID()}",
+    val id: String= "$WAREHOUSE_ID_PREFIX${Uuid.random()}",
     val name: String? = null,
     val regionalZone: String? = null,
     val latitude: Double? = null,
     val longitude: Double? = null
-)
+) {
+    companion object {
+        const val WAREHOUSE_ID_PREFIX = "WH-"
+    }
+}
